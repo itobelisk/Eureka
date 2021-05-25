e12c9dbc4f04efe5553ea5ef391891d5b89a1f3a
beautyit <noiroxc@gmail.com>
2021-05-25 18:21:50 +0400

    25-05-2021

--- src/main/java/com/eureka/server/EurekaServerApplication.java
+++ src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAsync
 public class EurekaServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
